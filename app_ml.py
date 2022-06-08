07a6dedab4b1ebadae5edcc1814bdcfb76ed5571
Yeo <guswls9281@gmail.com>
2022-06-09 00:04:14 +0900

    .

--- app_ml.py
+++ app_ml.py
@@ -37,5 +37,58 @@ def run_ml():
         st.write(y_pred)
     st.markdown("****")
     with st.expander('키워드 참고'):
-        img = Image.open('data/wc_jobdescription.png')
+ 
+        st.write('##### * 평점 1점의 Job Description 키워드')
+        img = Image.open('data/df1.png')
         st.image(img, use_column_width= True)
+        # # df 1점
+        # from wordcloud import WordCloud,STOPWORDS
+        # df_1 = df.loc[df['Rating'] == 1,]
+        # job_title=df_1['Job Description'][~pd.isnull(df_1['Job Description'])]
+        # wordCloud = WordCloud(width=450,height= 300,background_color= 'white').generate(' '.join(job_title))
+        # plt.figure(figsize=(19,9))
+        # plt.axis('off')
+        # plt.imshow(wordCloud)
+        # plt.show()
+        st.markdown('****')
+
+        st.write('##### * 평점 5점의 Job Description 키워드')
+        img = Image.open('data/df5.png')
+        st.image(img, use_column_width= True)
+        # # df 5점
+        # from wordcloud import WordCloud,STOPWORDS\
+        # df_5 = df.loc[df['Rating'] == 5,]
+        # job_title=df_5['Job Description'][~pd.isnull(df_5['Job Description'])]
+        # wordCloud = WordCloud(width=450,height= 300,background_color= 'white').generate(' '.join(job_title))
+        # plt.figure(figsize=(19,9))
+        # plt.axis('off')
+        # plt.imshow(wordCloud)
+        # plt.show()
+        st.markdown('****')
+
+        st.write('##### * 평점 1점과 5점의 Job Description 키워드')
+        img = Image.open('data/df1_5.png')
+        st.image(img, use_column_width= True)
+        # 평점 1점과 5점
+        # from wordcloud import WordCloud,STOPWORDS
+        # df_1_5 = df.loc[(df['Rating'] == 1) | (df['Rating'] == 5),]
+        # job_title=df_1_5 ['Job Description'][~pd.isnull(df_1_5 ['Job Description'])]
+        # my_stopwords = STOPWORDS
+        # my_stopwords.add('Job Description')
+        # wordCloud = WordCloud(width=450,height= 300,background_color= 'white',stopwords=my_stopwords).generate(' '.join(job_title))
+        # plt.figure(figsize=(19,9))
+        # plt.axis('off')
+        # plt.title(df_1_5 ['Job Description'].name,fontsize=20)
+        # plt.imshow(wordCloud)
+        # plt.show()
+        st.markdown('****')
+
+        st.write('##### * 전체 Job Description 키워드')
+        img = Image.open('data/df_description.png')
+        st.image(img, use_column_width= True)
+
+
+       
+
+
+
